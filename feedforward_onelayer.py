42fe4362f28c8a084b4ed1aac9733cddaee6fcf2
cwells1984 <thewilhelmscream@gmail.com>
2022-08-07 22:21:56 -0400

    Working 1-layer network

--- feedforward_onelayer.py
+++ feedforward_onelayer.py
@@ -53,18 +53,20 @@ class FeedForwardNetwork:
             self.forward_and_back_propagate(X_trn, y_trn, verbose=False)
 
             # Now check the performance
-            h1 = np.dot(X_trn, self.weights_input_hidden1)
-            out = np.dot(h1, self.weights_hidden1_output)
+            y_perf = []
+            for index in range(len(X_trn)):
+                net_h1, out_h1, net_o, out_o = self.network_forward(X_trn[index])
+                y_perf += [net_o]
 
             # Calculate score
             if (self.type == 'Classifier'):
-                score = eval.eval_softmax(y_trn, out)
+                score = eval.eval_softmax(y_trn, y_perf)
                 if score > last_score:
                     last_score = score
                 else:
                     optimal_score_reached = True
             else:
-                score = eval.eval_mse(y_trn, out)[0]
+                score = eval.eval_mse(y_trn, y_perf)[0]
                 if score < last_score:
                     last_score = score
                 else:
@@ -78,8 +80,11 @@ class FeedForwardNetwork:
             df_X_tst = df_X_tst.loc[:, df_X_tst.columns != label_column]
         X_tst = df_X_tst.values
 
-        h1 = np.dot(X_tst, self.weights_input_hidden1)
-        y_pred = np.dot(h1, self.weights_hidden1_output)
+        y_pred = []
+        for index in range(len(X_tst)):
+            net_h1, out_h1, net_o, out_o = self.network_forward(X_tst[index])
+            y_pred += [net_o]
+
         return y_pred, y_truth
 
     def fit_predict(self, df_trn, df_tst, label_columns):
@@ -100,34 +105,38 @@ class FeedForwardNetwork:
     # https://blog.yani.ai/deltarule/
     def forward_and_back_propagate(self, X_trn, y_trn, verbose=False):
 
-        # Conducts the forward and back propagation thru the 1-layer neural network fed by the encoder
-        def forward_and_back_propagate(self, X_trn, y_trn, verbose=False):
+        # For each sample from the training data
+        for index in range(len(X_trn)):
 
-            # For each sample from the training data
-            for index in range(len(X_trn)):
+            # Calculate updated weights in a new matrix - we want the originals for further back-propagation
+            delta_weights_input_hidden1 = np.zeros((self.num_columns, self.num_hidden1))
+            delta_weights_hidden1_output = np.zeros((self.num_hidden1, self.num_outputs))
 
-                # Calculate updated weights in a new matrix - we want the originals for further back-propagation
-                delta_weights_input_hidden1 = np.zeros((self.num_columns, self.num_hidden1))
-                delta_weights_hidden1_output = np.zeros((self.num_hidden1, self.num_outputs))
+            # First forward thru the network
+            net_h1, out_h1, net_o, out_o = self.network_forward(X_trn[index])
 
-                # First forward thru the network
-                net_h1, out_h1, net_o, out_o = self.network_forward(X_trn[index])
+            # Back Propagate Output -> V weights
+            deriv_and_error = (y_trn[index] - out_o) * out_o * (1 - out_o)
+            for h in range(self.num_hidden1):
+                for o in range(self.num_outputs):
+                    delta_weights_hidden1_output[h][o] = -1 * deriv_and_error[o] * out_h1[h]
 
-                # Back Propagate Output -> Hidden Layer 1
-                deriv_and_error = (y_trn[index] - out_o) * out_o * (1 - out_o)
+            # Back propagate Hidden Layer 1 -> Input
+            error_h1 = np.zeros(self.num_hidden1)
+            for h in range(len(error_h1)):
+                error_h1[h] = np.dot(deriv_and_error, self.weights_hidden1_output[h, :])
+            deriv_out_h1 = (out_h1 * (1 - out_h1))
+            for i in range(self.num_columns):
                 for h in range(self.num_hidden1):
-                    for o in range(self.num_outputs):
-                        delta_weights_hidden1_output[h][o] = -1 * deriv_and_error[o] * out_h1[h]
+                    delta_weights_input_hidden1[i][h] = -1 * error_h1[h] * deriv_out_h1[h] * X_trn[index, i]
 
-                # Back propagate Hidden Layer 1 -> Input
-                error_h1 = np.zeros(self.num_hidden1)
-                for h in range(len(error_h1)):
-                    error_h1[h] = np.dot(deriv_and_error, self.weights_hidden1_output[h, :])
-                deriv_out_h1 = (out_h1 * (1 - out_h1))
-                for i in range(self.num_columns):
-                    for h in range(self.num_hidden1):
-                        delta_weights_input_hidden1[i][h] = -1 * error_h1[h] * deriv_out_h1[h] * X_trn[index][i]
+            # New backpropagation code
+            #deriv_h1 = out_h1 * (1 - out_h1)
+            #error_out = out_o - y_trn[index]
+            #error_h1 = np.dot(error_out, self.weights_hidden1_output.T) * deriv_h1
+            #delta_weights_hidden1_output = np.dot(np.array([out_h1]).T, np.array([error_out]))
+            #delta_weights_input_hidden1 = np.dot(np.array([X_trn[index]]).T, np.array([error_h1]))
 
-                # Now that the entire training set is run through, update
-                self.weights_encoding_hidden1 -= self.learn_rate * delta_weights_input_hidden1
-                self.weights_hidden1_output -= self.learn_rate * delta_weights_hidden1_output
+            # Now that the entire training set is run through, update
+            self.weights_input_hidden1 = self.weights_input_hidden1 - self.learn_rate * delta_weights_input_hidden1
+            self.weights_hidden1_output = self.weights_hidden1_output - self.learn_rate * delta_weights_hidden1_output
